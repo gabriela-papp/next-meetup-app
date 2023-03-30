09cf74c80f133ac28400392e792f4925095d00e9
Gabriela Papp <gpgabika@gmail.com>
2023-03-30 23:06:03 +0100

    getting data from Mongo db

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,50 @@
-import {MongoClient} from 'mongodb'
+import { MongoClient } from 'mongodb'
 
 import MeetupList from '../components/meetups/MeetupList'
 
-
-const DUMMY_DATA=[
-    {
-        id:'m1',
-        title:'First Local Meetup',
-        image:'https://images.unsplash.com/photo-1550340499-a6c60fc8287c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        address:'123 Some Street, Some Town',
-        description:'First MeetUp'
-    },{
-          id:'m2',
-        title:'Second Local Meetup',
-        image:'https://images.unsplash.com/photo-1529260830199-42c24126f198?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
-        address:'234 Another Street, Another Town',
-        description:'Second Meetup'
-    }
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'First Local Meetup',
+    image:
+      'https://images.unsplash.com/photo-1550340499-a6c60fc8287c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    address: '123 Some Street, Some Town',
+    description: 'First MeetUp',
+  },
+  {
+    id: 'm2',
+    title: 'Second Local Meetup',
+    image:
+      'https://images.unsplash.com/photo-1529260830199-42c24126f198?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
+    address: '234 Another Street, Another Town',
+    description: 'Second Meetup',
+  },
 ]
 
-
-
 function Home(props) {
-  return (
-      <MeetupList meetups={props.meetups}/>
-  )
-  
+  return <MeetupList meetups={props.meetups} />
 }
-export async function getStaticProps(){
-    return{
-        props:{
-            meetups:DUMMY_DATA
-        }
-    }
+
+export async function getStaticProps() {
+    const client = await MongoClient.connect('mongodb+srv://gpgabika:Apqe0B1tezbGHfj1@cluster0.0n2z00u.mongodb.net/meetups?retryWrites=true&w=majority')
+         const db =client.db()
+
+         const meetupsCollection = db.collection('meetups')
+
+         const meetups = await  meetupsCollection.find().toArray()
+         client.close()
+
+  return {
+    props: {
+      meetups: meetups.map(meetup=>({
+        title:meetup.title,
+        address:meetup.address,
+        image:meetup.image,
+        id:meetup._id.toString()
+      })),
+    },
+    revalidate: 1,
+  }
 }
 
 export default Home
